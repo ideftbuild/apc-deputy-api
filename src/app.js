6e6c816ede80a32db339e8e989e9ba9863b9f124
ideftbuild <a.akanSWE@gmail.com>
2025-11-06 19:13:22 +0100

    feat: add cors support

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 import express from "express";
+import cors from "cors";
 import contactRoute from "./routes/contact.route.js";
 
 const app = express();
 
+// Middleware
+app.use(
+  cors({
+    origin: ["http://localhost:5173", "https://apc-deputy-landing.vercel.app"],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(express.json());
 app.use("/contact", contactRoute); // ✅ mount the route on the main app
 
