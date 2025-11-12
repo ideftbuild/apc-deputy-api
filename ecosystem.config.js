053fffc955597696eee6792424afe648f6398532
ideftbuild <a.akanSWE@gmail.com>
2025-11-12 10:58:25 +0100

    chore: add PM2 ecosystem.config.js for cluster and environment management

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+export default {
+  apps: [
+    {
+      name: "myapp",
+      script: "./server.js",
+      instances: "max",
+      exec_mode: "cluster",
+      env: {
+        NODE_ENV: "development",
+        PORT: 3000,
+      },
+      env_production: {
+        NODE_ENV: "production",
+        PORT: 8000,
+      },
+    },
+  ],
+};
